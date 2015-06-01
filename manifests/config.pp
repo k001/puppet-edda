b1a6a326a65fd55cbba357f9668d7d57f686e3a5
Ivan Zenteno <k001.operator@gmail.com>
2015-06-01 00:08:11 -0500

    Adding source for module

--- manifests/config.pp
+++ manifests/config.pp
@@ -7,7 +7,7 @@ class edda::config {
         source_url => 'http://mirror.nexcess.net/apache/tomcat/tomcat-8/v8.0.23/bin/apache-tomcat-8.0.23.tar.gz',
         catalina_base => '/usr/local/tomcat',
     }->
-    tomcat::service { 'default': }
+    tomcat::service { 'default': }->
     tomcat::war { 'edda-2.1.war':
         catalina_base => '/usr/local/tomcat',
         war_source => 'http://s3.amazonaws.com/edda-bucket/dev/war/edda-2.1.war',
