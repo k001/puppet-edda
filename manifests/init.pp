711fd587877bcb48f1d167ff78af2d7b826c3894
Ivan Zenteno <k001.operator@gmail.com>
2015-06-01 13:02:47 -0500

    Adding source for module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,6 @@
 class edda {
-  anchor {'edda::begin':}   ->
   class {'edda::install':}  ->
   class {'edda::config':}   ->
-  class {'edda::service':}  ->
-  anchor {'edda::end':}
+  class {'edda::service':}
 }
 class {'edda':}
