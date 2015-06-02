0d6ecfbf2bfa176828a1585edaee99167f1e4003
Ivan Zenteno <k001.operator@gmail.com>
2015-06-02 16:06:40 -0500

    Adding parameters to master class *Fixing typo

--- edda/manifests/params.pp
+++ edda/manifests/params.pp
@@ -3,7 +3,7 @@ class edda::params {
   $catalina_base     = '/opt/www/tomcat8'
   $start_command     = "${catalina_base}/bin/startup.sh"
   $stop_command      = "${catalina_base}/bin/shutdown.sh"
-  $war_source        = "http://s3.amazonaws.com/edda-bucket/dev/war/edda-2.1.war"
+  $war_source        = 'http://s3.amazonaws.com/edda-bucket/dev/war/edda-2.1.war'
   $war_version       = '2.1'
   $tomcat_url_source = 'http://mirror.nexcess.net/apache/tomcat/tomcat-8/v8.0.23/bin/apache-tomcat-8.0.23.tar.gz'
 }
